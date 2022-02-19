8977be95c7d779fb6b13b633d61287a7f0d6bca2
haris <haris@LAPTOP-FMO9C68M>
2022-02-19 21:51:34 +0000

    Added new project for supervisor code

---
+++ Services/Managers/SupivisorManager.cs
@@ -0,0 +1,37 @@
+﻿using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Managers
+{
+    public class SupivisorManager : ISupervisorManager
+    {
+        public SupivisorManager()
+        {
+
+        }
+
+        public void CheckAllBins(List<FeedBin> feedBins)
+        {
+
+        }
+
+        public void MakeBatch()
+        {
+
+        }
+
+        public void ReportBatch()
+        {
+           
+        }
+
+        public void CheckBatch(List<FeedBin> feedBins)
+        {
+
+        }
+    }
+}
---
+++ Services/Models/Batch.cs
@@ -0,0 +1,16 @@
+﻿using Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Models
+{
+    public class Batch
+    {
+        public int Id { get; set; }
+        public Recipe Recipe { get; set; }
+        public double Amount { get; set; }
+    }
+}
