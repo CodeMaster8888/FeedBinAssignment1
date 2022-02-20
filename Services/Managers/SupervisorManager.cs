e0e2ddd97b418f4989e896d5212b8538a8dabad2
haris <haris@LAPTOP-FMO9C68M>
2022-02-20 16:42:27 +0000

    Added CheckBatch method and test

---
+++ Services/Managers/SupervisorManager.cs
@@ -0,0 +1,75 @@
+﻿using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Managers
+{
+    public class SupervisorManager : ISupervisorManager
+    {
+        public SupervisorManager()
+        {
+
+        }
+
+        public void MakeBatch()
+        {
+
+        }
+
+        public void ReportBatch()
+        {
+           
+        }
+
+        public bool CheckBatch(List<FeedBin> feedBins, Batch batch)
+        {
+            var (firstValue, secondValue) = CalculateValues(batch);
+
+
+            var temp = feedBins.Select(x => {
+                
+                if(x.ProductName == batch.Recipe.FirstIngredient.ProductName) 
+                {
+                    if (CanRemove(x, firstValue))
+                    {
+                        return true;
+                    }
+                }else if (x.ProductName == batch.Recipe.SecondIngredient.ProductName)
+                {
+                    if (CanRemove(x, firstValue))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            });
+
+            var count =  temp.Count(x => x == true);
+
+            return count == 2;
+        }
+
+        private (double, double) CalculateValues(Batch batch)
+        {
+            var firstPercentage = batch.Recipe.FirstPercentage;
+            var secondPercentage = batch.Recipe.SecondPercentage;
+
+            var firstValueTaken = firstPercentage * batch.Amount;
+            var secondValueTaken = secondPercentage * batch.Amount;
+
+            return (firstValueTaken, secondValueTaken);
+        }
+
+        private bool CanRemove(FeedBin feedBin, double valueToRemove)
+        {
+            if(feedBin.CurrentVolume >= valueToRemove)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ UnitTesting/Services/Managers/SupervisorManagerTests.cs
@@ -0,0 +1,69 @@
+﻿using Database.Models;
+using Services.Managers;
+using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTesting.Services.Managers
+{
+    public class SupervisorManagerTests
+    {
+        ISupervisorManager supervisorManager = new SupervisorManager();
+        List<FeedBin> feedBins;
+        Batch Batch;
+
+        public SupervisorManagerTests()
+        {
+            feedBins = new List<FeedBin>
+            {
+                new FeedBin
+                {
+                    BinNumber = 1,
+                    CurrentVolume = 20,
+                    MaxVolume = 40,
+                    ProductName = "Test1"
+                },
+                new FeedBin
+                {
+                    BinNumber = 2,
+                    CurrentVolume = 20,
+                    MaxVolume = 40,
+                    ProductName = "Chicken"
+                },
+                new FeedBin
+                {
+                    BinNumber = 3,
+                    CurrentVolume = 20,
+                    MaxVolume = 40,
+                    ProductName = "Fruit"
+                }
+            };
+            Batch = new Batch
+            {
+                Amount = 20,
+                Id = 1,
+                Recipe = new Recipe
+                {
+                    Id = 1,
+                    Name = "TestFood",
+                    FirstIngredient = new Ingredient { Id = 1, ProductName = "Chicken"},
+                    SecondIngredient = new Ingredient { Id = 2, ProductName = "Fruit" },
+                    FirstPercentage = 0.7,
+                    SecondPercentage = 0.3
+                }
+            };
+        }
+
+        [Fact]
+        public void test1()
+        {
+            var result = supervisorManager.CheckBatch(feedBins, Batch);
+
+            Assert.True(result);
+        }
+    }
+}
